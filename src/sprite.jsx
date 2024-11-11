791463421eb4bf4ee28cc52f23891409ef910816
VarshanUtha <varshansutha916@gmail.com>
2024-11-10 17:44:27 -0800

    modified:   src/assets/sprites/playableJump.PNG 	modified:   src/sprite.jsx

--- src/sprite.jsx
+++ src/sprite.jsx
@@ -4,6 +4,7 @@ class Sprites extends Phaser.Scene {
   constructor() {
     super({ key: "Sprites" });
     this.player = null;
+    this.cursors = null;
   }
 
   preload() {
@@ -12,7 +13,14 @@ class Sprites extends Phaser.Scene {
       frameWidth: 150,
       frameHeight: 150,
     });
-
+    this.load.spritesheet("dudeRun", "src/assets/sprites/playableRun.png", {
+      frameWidth: 150,
+      frameHeight: 150,
+    });
+    this.load.spritesheet("dudeJump", "src/assets/sprites/playableJump.PNG",{
+      frameWidth: 150,
+      frameHeight: 150
+    });
   }
   
   create() {
@@ -27,9 +35,47 @@ class Sprites extends Phaser.Scene {
       frameRate: 8,
       repeat: -1,
     });
+    this.anims.create({
+      key: "runLeft",
+      frames: this.anims.generateFrameNumbers("dudeRun", { start: 0, end: 4 }),
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "runRight",
+      frames: this.anims.generateFrameNumbers("dudeRun", { start: 0, end: 4 }),
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "jump",
+      frame: [{key: "dudeJump", frame: 0}],
+      frameRate: 20
+    });
+    this.anims.create({
+      key: "fall",
+      frame: [{key: "dudeJump", frame: 1}],
+      frameRate: 20
+    });
 
-    this.player.anims.play("idle", true);
+    this.cursors = this.input.keyboard.createCursorKeys();
+
   }
-}
 
+  update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-180);
+      this.player.anims.play("runLeft", true);
+      this.player.flipX = false;
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(180);
+      this.player.anims.play("runRight", true);
+      this.player.flipX = true;
+    } else {
+      this.player.setVelocityX(0);
+      this.player.anims.play("idle", true);
+    }
+
+  } 
+}
 export default Sprites;
